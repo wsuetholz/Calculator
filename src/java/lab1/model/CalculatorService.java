cc2109c59caccbdb5ca0436ece2fb1c59e585208
Bill S. <wj@suetholz.net>
2015-01-28 20:53:09 -0600

    made calculator service be serializable.

--- src/java/lab1/model/CalculatorService.java
+++ src/java/lab1/model/CalculatorService.java
@@ -5,14 +5,45 @@
  */
 package lab1.model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author wsuetholz
  */
-public class CalculatorService {
+public class CalculatorService implements Serializable {
+    static final long serialVersionUID = 42L;
+    static long idGen = 1;
+    private long myId;
     
+    public CalculatorService () {
+	myId = idGen++;
+    }
+        
     public long calculateRectangleArea ( long length, long width ) {
 	return (length * width);
     }
-    
+
+    @Override
+    public int hashCode() {
+	int hash = 7;
+	hash = 71 * hash + (int) (this.myId ^ (this.myId >>> 32));
+	return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (obj == null) {
+	    return false;
+	}
+	if (getClass() != obj.getClass()) {
+	    return false;
+	}
+	final CalculatorService other = (CalculatorService) obj;
+	if (this.myId != other.myId) {
+	    return false;
+	}
+	return true;
+    }
+        
 }
